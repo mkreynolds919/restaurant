ee22ed4f344a17e91d29b6ab755d59a63d1bb5e6
mkreynolds919 <mkreynolds919@gmail.com>
2025-02-03 09:55:38 -0600

    Create assets/images folder
    Insert main logo image
    
    Create IIFE for logo image in index.js
    
    Style nav buttons to remove default styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './styles.css';
+import kikiandbouba from './assets/images/kikiandbouba.png';
 
-console.log("YOOOOO");
+(() => {
+    const mainLogo = document.createElement("img");
+    mainLogo.src = kikiandbouba;
+    document.body.appendChild(mainLogo);
+})();
+
