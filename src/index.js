303887ec24ede63062d03273a88b02f317ca734d
mkreynolds919 <mkreynolds919@gmail.com>
2025-02-03 10:05:32 -0600

    Add id to logo image
    Use absolute positioning for main logo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import kikiandbouba from './assets/images/kikiandbouba.png';
 (() => {
     const mainLogo = document.createElement("img");
     mainLogo.src = kikiandbouba;
+    mainLogo.id = "main-logo";
     document.body.appendChild(mainLogo);
 })();
 
