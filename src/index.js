90eedbf5c61b9de0ec17e78ac56ff6a98dbc3757
mkreynolds919 <mkreynolds919@gmail.com>
2025-02-03 11:20:16 -0600

    Create cleanPage function in index.js for tab-switching functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './styles.css';
 import kikiandbouba from './assets/images/kikiandbouba.png';
 
-
+function cleanPage() {
+    const contents = document.querySelectorAll('#content *');
+    contents.forEach((element) => {
+        element.remove;
+    });
+}
 
 (() => {
     const mainLogo = document.createElement("img");
