642c8f68c267bff993159d733b9850b08a570101
mkreynolds919 <mkreynolds919@gmail.com>
2025-02-07 19:02:33 -0600

    Create and add "new arrivals" and "reviews" divs on either side of splash image on homepage

--- src/homepage.js
+++ src/homepage.js
@@ -2,11 +2,21 @@ import homepageSplash from './assets/images/homepagesplash.jpeg';
 import './homepage.css';
 
 const homepageLoad = function() {
+
     const content = document.querySelector("#content");
+
     const splashImg = document.createElement("img");
     splashImg.src = homepageSplash;
     splashImg.id = 'homepage-splash';
     content.appendChild(splashImg);
+
+    const newArrivals = document.createElement("div");
+    content.insertBefore(newArrivals, splashImg);
+
+    const reviews = document.createElement("div");
+    content.appendChild(reviews);
+
+   
 }
 
 export default homepageLoad;
